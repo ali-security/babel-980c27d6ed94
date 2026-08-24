abd2ebf3e6b1df5b6789d12bf97ff6b6f7d90183
Seal <info@sealsecurity.io>
2026-08-24 10:25:18 +0000

    CVE-2026-44728

--- packages/babel-plugin-transform-modules-systemjs/src/index.ts
+++ packages/babel-plugin-transform-modules-systemjs/src/index.ts
@@ -94,15 +94,11 @@ function constructExportCall(
       const objectProperties = [];
       for (let i = 0; i < exportNames.length; i++) {
         const exportName = exportNames[i];
+        const exportNameNode = stringSpecifiers.has(exportName)
+          ? t.stringLiteral(exportName)
+          : t.identifier(exportName);
         const exportValue = exportValues[i];
-        objectProperties.push(
-          t.objectProperty(
-            stringSpecifiers.has(exportName)
-              ? t.stringLiteral(exportName)
-              : t.identifier(exportName),
-            exportValue,
-          ),
-        );
+        objectProperties.push(t.objectProperty(exportNameNode, exportValue));
       }
       statements.push(
         t.expressionStatement(
@@ -129,6 +125,10 @@ function constructExportCall(
 
     for (let i = 0; i < exportNames.length; i++) {
       const exportName = exportNames[i];
+      const computed = stringSpecifiers.has(exportName);
+      const exportNameNode = computed
+        ? t.stringLiteral(exportName)
+        : t.identifier(exportName);
       const exportValue = exportValues[i];
 
       statements.push(
@@ -137,7 +137,8 @@ function constructExportCall(
             "=",
             t.memberExpression(
               t.identifier(exportObj),
-              t.identifier(exportName),
+              exportNameNode,
+              computed,
             ),
             exportValue,
           ),
